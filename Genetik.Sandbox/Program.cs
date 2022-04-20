bb268740fea031712fc669627048db6b1b32db9d
Alexander Blazhkov <thermo911@yandex.ru>
2022-04-20 17:01:40 +0300

    chore: sandbox

---
+++ Genetik.Sandbox/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Genetik.Evolution.Tools;
+
+var random = new Random(Guid.NewGuid().GetHashCode());
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(random.NextDistinctPair(3, 6));
+}
